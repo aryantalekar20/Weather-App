cb5bd0b5722814c3a833ee64871aa6cfd55e9a17
arytalekps <arytalek@publicisgroupe.net>
2023-06-22 15:51:37 -0400

    added a Celsius and Fahrenheit toggle and fixed minor bugs
    changed background image specs to fit full screen

--- src/script.js
+++ src/script.js
@@ -25,14 +25,14 @@ let weather = {
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°F";
+    document.querySelector(".temp").innerText = Math.round(temp) + "°F";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText =
       "Wind speed: " + speed + " mph";
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?," + name + "')";
+      "url('https://source.unsplash.com/1920x1080/?," + name + "')";
       console.log("name: " + name);
   },
   search: function () {
