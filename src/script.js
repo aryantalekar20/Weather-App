bfcfcf534d5a66c42cdabd1d18fbb1a017de9cb2
arytalekps <arytalek@publicisgroupe.net>
2023-06-23 10:03:04 -0400

    semiworking weather switcher

--- src/script.js
+++ src/script.js
@@ -16,6 +16,23 @@ let weather = {
       })
       .then((data) => this.displayWeather(data));
   },
+  fetchWeatherMetric: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+
   displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
@@ -36,7 +53,14 @@ let weather = {
       console.log("name: " + name);
   },
   search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
+    if(toggle){
+      this.fetchWeather(document.querySelector(".search-bar").value);
+      console.log(toggle);
+    }
+    else{
+      this.fetchWeatherMetric(document.querySelector(".search-bar").value);
+      console.log(toggle);
+    }
   },
 };
 
@@ -52,4 +76,99 @@ document
     }
   });
 
+
+
+const apiKey = "58cbf43603328cda648836bbdd9dd87c";
+
+function displayWeatherF(data) {
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const { temp, humidity } = data.main;
+  const { speed } = data.wind;
+  document.querySelector(".city").innerText = "Weather in " + name;
+  document.querySelector(".icon").src =
+    "https://openweathermap.org/img/wn/" + icon + ".png";
+  document.querySelector(".description").innerText = description;
+  document.querySelector(".temp").innerText = Math.round(temp) + "°F";
+  document.querySelector(".humidity").innerText =
+    "Humidity: " + humidity + "%";
+  document.querySelector(".wind").innerText =
+    "Wind speed: " + speed + " mph";
+  document.querySelector(".weather").classList.remove("loading");
+  document.body.style.backgroundImage =
+    "url('https://source.unsplash.com/1920x1080/?," + name + "')";
+    console.log("name: " + name);
+}
+
+function displayWeatherC(data) {
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const { temp, humidity } = data.main;
+  const { speed } = data.wind;
+  document.querySelector(".city").innerText = "Weather in " + name;
+  document.querySelector(".icon").src =
+    "https://openweathermap.org/img/wn/" + icon + ".png";
+  document.querySelector(".description").innerText = description;
+  document.querySelector(".temp").innerText = Math.round(temp) + "°C";
+  document.querySelector(".humidity").innerText =
+    "Humidity: " + humidity + "%";
+  document.querySelector(".wind").innerText =
+    "Wind speed: " + speed + " kmph";
+  document.querySelector(".weather").classList.remove("loading");
+  document.body.style.backgroundImage =
+    "url('https://source.unsplash.com/1920x1080/?," + name + "')";
+    console.log("name: " + name);
+}
+
+function fetchWeather(city){
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=" +
+      apiKey
+  )
+    .then((response) => {
+      if (!response.ok) {
+        alert("No weather found.");
+        console.log(response);
+        throw new Error("No weather found.");
+      }
+      return response.json();
+    })
+    .then((data) => displayWeatherF(data));
+}
+
+
+function fetchWeatherMetric(city) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=metric&appid=" +
+      apiKey
+  )
+    .then((response) => {
+      if (!response.ok) {
+        alert("No weather found.");
+        throw new Error("No weather found.");
+      }
+      return response.json();
+    })
+    .then((data) => displayWeatherC(data));
+}
+
+
+
+let toggle = true;
+function func() {
+ toggle = !toggle;
+ if(toggle){
+  fetchWeather(document.querySelector(".search-bar").value);
+ }
+ else{
+  fetchWeatherMetric(document.querySelector(".search-bar").value);
+ }
+}
+const tog = document.getElementById("toggleID");
+tog.addEventListener('click', func);
 weather.fetchWeather("Boston");
+
